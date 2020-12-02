be8bc5619d389ff66777eb73d1c384c642fe6b13
Rijvan Mohammad <rijvanmohd007@gmail.com>
2020-12-02 22:59:21 +0530

    Update Day1-Report-Repair.py

--- Day1-Report-Repair.py
+++ Day1-Report-Repair.py
@@ -1,17 +1,17 @@
-#The Elves in accounting just need you to fix your expense report (your puzzle input); apparently, something isn't quite adding up.
+'''The Elves in accounting just need you to fix your expense report (your puzzle input); apparently, something isn't quite adding up.
 
-#Specifically, they need you to find the two entries that sum to 2020 and then multiply those two numbers together.
+Specifically, they need you to find the two entries that sum to 2020 and then multiply those two numbers together.
 
-#For example, suppose your expense report contained the following:
+For example, suppose your expense report contained the following:
 
-#1721
-#979
-#366
-#299
-#675
-#1456
-#In this list, the two entries that sum to 2020 are 1721 and 299. Multiplying them together produces 1721 * 299 = 514579, so the correct answer is 514579.
-
+1721
+979
+366
+299
+675
+1456
+In this list, the two entries that sum to 2020 are 1721 and 299. Multiplying them together produces 1721 * 299 = 514579, so the correct answer is 514579.
+'''
 
 def find_product_of_two(arr,total):
   s = set()
@@ -20,3 +20,21 @@ def find_product_of_two(arr,total):
     if tmp in s:
       print(tmp * each)
     s.add(tmp)
+    
+'''
+The three entries that sum to 2020 are 979, 366, and 675. Multiplying them together produces the answer, 241861950.
+
+In your expense report, what is the product of the three entries that sum to 2020?
+
+'''
+    
+def find_product_of_triplets(arr,total):
+  for i in range(0, len(arr)-1): 
+      # Find pair in subarray A[i + 1..n-1]  
+      # with sum equal to sum - A[i] 
+      s = set() 
+      curr_sum = total - arr[i] 
+      for j in range(i + 1, len(arr)): 
+          if (curr_sum - arr[j]) in s: 
+              print((curr_sum-arr[j]) * arr[i] * arr[j])
+          s.add(arr[j])
